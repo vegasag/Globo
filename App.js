aa1b310d456e2c78f4b7be0b1aa5762eeeee1d26
Vegard Saga <vegard.saga@gmail.com>
2019-03-14 14:36:27 +0100

    Removes yellow debugging warnings to increase speed and reduce processing power

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Home } from './app/views/Home.js';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings(['Remote debugger']);
 
 
 export default class App extends React.Component {
