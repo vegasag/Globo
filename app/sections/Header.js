840980814a2d0742df68d85a6e968cfdcc45bea3
Vegard Saga <vegard.saga@gmail.com>
2019-03-14 14:48:48 +0100

    Inserts a logo in the header and laptop picture in the middle of the homepage

--- app/sections/Header.js
+++ app/sections/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export class Header extends React.Component {
     constructor(props){
@@ -17,6 +17,10 @@ export class Header extends React.Component {
         let display = this.state.isLoggedIn ? 'Sample User' : this.props.message;
         return (
             <View style={styles.headStyle}>
+                <Image 
+                    style={styles.logoStyle}
+                    source={require('./img/Globo_logo_REV.png')}
+                />
                 <Text 
                     style={styles.headText}
                     onPress={this.toggleUser}>{display}</Text>
@@ -30,11 +34,20 @@ const styles = StyleSheet.create({
         textAlign: 'right',
         color: '#ffffff',
         fontSize: 20,
+        flex: 1,
     },
     headStyle: {
         paddingTop: 50,
         paddingRight: 10,
         backgroundColor: '#35605a',
         flex: 1,
-    }
+        flexDirection: 'row',
+        borderBottomWidth: 2,
+        borderColor: '#000000', 
+    },
+    logoStyle: {
+        flex: 1,
+        width: undefined,
+        height: undefined,
+    },
 });
