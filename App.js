f15d7df415c5364e9d420927956c877648ed2fb4
Vegard Saga <vegard.saga@gmail.com>
2019-03-14 11:40:22 +0100

    Refactors folder structure

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './app/views/Home.js';
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Home/>
+      // <View style={styles.container}>
+      //   <Text>Open up App.js to start working on your app!</Text>
+      // </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ app/views/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export class Home extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text> Test </Text>
+                <Text> Test2 </Text>
+                <Text> Test3 </Text>
+            </View>
+        );
+    }
+}
