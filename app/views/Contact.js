644aaab74b66bbb76a54c00698350269a791ee42
Vegard Saga <vegard.saga@gmail.com>
2019-03-14 17:24:10 +0100

    Adds textinput fields with feedback message

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -1,27 +1,103 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableHighlight, Alert, Keyboard } from 'react-native';
 import { Header } from '../sections/Header.js';
+import { StackNavigator } from 'react-navigation';
 
 export class Contact extends React.Component {
-    static navigationOptions = {
-        header: null,
+	static navigationOptions = {
+      		header: null
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            msg: 'Enter Message',
+            name: 'Enter Name',
+            email: 'Enter your Email Address'
+         }    
     }
 
-    render(){
+    clearFields=()=> this.setState({name:'',msg:'', email:''});
+
+    sendMessage=()=> { 
+        Alert.alert(this.state.name,this.state.msg);
+        this.props.navigation.goBack();
+    };
+
+
+    render() {
         const { goBack } = this.props.navigation;
+
         return (
-            <View style={styles.container}>
-                <Header message = 'Press to Login' />
-                <Text style={{flex: 8}}>The contact form will go here</Text>
-                <Text style={{flex: 6}}>More contact form will go here</Text>
-                <Button onPress={() => goBack()} title="Go back to homescreen" />
-            </View>
-        )
+          <View style={styles.container}>
+            <Header message = 'Press to Login' />
+            <Text style={styles.heading}>Contact Us</Text>
+              
+            <TextInput 
+                style={styles.inputs} 
+                onChangeText={(text) => this.setState({name: text})}
+                value={this.state.name}
+            />
+
+            <TextInput 
+                style={styles.multiInput} 
+                onChangeText={(text) => this.setState({msg: text})}
+                value={this.state.msg}
+                multiline = {true}
+                numberOfLines = {4}
+            />
+
+            <TextInput 
+                style={styles.inputs} 
+                onChangeText={(text) => this.setState({email: text})}
+                value={this.state.email}
+            />
+
+            <TouchableHighlight onPress={this.sendMessage} underlayColor='#31e981'>
+                <Text style = {styles.buttons}>
+                    Send Message
+                </Text>
+            </TouchableHighlight>
+            
+            <TouchableHighlight onPress={this.clearFields} underlayColor='#31e981'>
+              <Text style = {styles.buttons}>
+                Reset Form
+              </Text>
+            </TouchableHighlight>
+
+            <Button onPress={() => goBack()} title="Go back to homescreen" />
+
+          </View>
+        );
     }
-}
+};
+
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'center',
+        paddingBottom: '45%'
+    },
+    heading: {
+        fontSize: 16,
+        flex: 1
+    },
+    inputs:{
+        flex: 1,
+        width: '80%',
+        padding: 10
+    },
+    multiInput:{
+        flex: 2,
+        width: '90%',
+        paddingTop:20
+    },
+    buttons:{
+        marginTop: 15,
+        fontSize: 16
     }
 });
+
+
+
